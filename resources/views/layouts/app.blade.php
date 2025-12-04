c4e809f8f20532d6e9ce7b7aeecf968d5d6f7a67
German Romero <germanrole98@gmail.com>
2025-12-04 09:14:06 -0500

    Añadir logotipos y actualizar el diseño de la cabecera

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TE APOYAMOS SAS')</title>
 
+
+    <!-- CDN de Bootstrap CSS -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+
     @vite('resources/css/app.css')
 
 </head>
 <body>
 
     <header class="header">
+
         <div class="nav-container">
+
             <div class="logo-container">
-                <div class="logo">TE</div>
-                <div class="logo">APOYAMOS</div>
-                <div class="logo">SAS</div>
+                <div class="logo-img">
+                    <img src="{{asset('img/logo_titulo.png')}}" alt="logo">
+                </div>
             </div>
             
             <div class="nav-links">
@@ -57,5 +65,9 @@
             </div>
         </div>
     </footer>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 </body>
 </html>
